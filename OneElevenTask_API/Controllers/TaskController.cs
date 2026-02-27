9b47e3187b1fb423a18afa4f89daa8c8be968eda
Lindele Nyambe <lxnyambe@gmail.com>
2026-02-27 12:43:28 +0200

    Initial commit: One Eleven Task Completed

---
+++ OneElevenTask_API/Controllers/TaskController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OneElevenTask.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        public class WebhookRequest
+        {
+            public string Data { get; set; } = null!;
+        }
+
+        public class WebhookResponse
+        {
+            public char[] Word { get; set; } = null!;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] WebhookRequest request)
+        {
+            if (request == null || string.IsNullOrEmpty(request.Data))
+            {
+                return BadRequest(new { message = "The data field is required." });
+            }
+
+            char[] charArray = request.Data.ToCharArray();
+
+            Array.Sort(charArray);
+
+            return Ok(new WebhookResponse { Word = charArray });
+        }
+    }
+}
---
+++ OneElevenTask_API/Program.cs
@@ -0,0 +1,32 @@
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
